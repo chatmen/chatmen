4b995f388c18d52321551d3c5aceb9503fd7d9d6
fuma_tsuchiya <fuma.tsuchiya@nextbeat.net>
2019-06-28 14:58:46 +0900

    画面遷移の一時的変更

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -16,7 +16,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * Create an Action to render an HTML page.
    *
    * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
+   * will be called nwhen the application receives a `GET` request with
    * a path of `/`.
     */
   
